ddcc8deb3a28524b094fe85f1347d35f732382b6
wagenrace <nijhof.dns@gmail.com>
2019-08-15 11:37:22 +0200

    Bug fix: Don't pick anchor 3 twice while anchor 0 never

--- yolo3/model.py
+++ yolo3/model.py
@@ -217,7 +217,7 @@ def yolo_eval(yolo_outputs, anchors, num_classes, image_shape, max_boxes=20,
     """Evaluate YOLO model on given input and return filtered boxes."""
     num_layers = len(yolo_outputs)
     anchor_mask = [[6, 7, 8], [3, 4, 5], [0, 1, 2]] \
-        if num_layers == 3 else [[3, 4, 5], [1, 2, 3]]  # default setting
+        if num_layers == 3 else [[3, 4, 5], [0, 1, 2]]  # default setting
     input_shape = K.shape(yolo_outputs[0])[1:3] * 32
     boxes = []
     box_scores = []
